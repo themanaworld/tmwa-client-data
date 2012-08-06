74cae7e4507a659c49fd3168099fadf28b4e5259
tux9th <mr.x@aon.at>
2012-08-06 15:14:35 +0200

    added Crypt-Fringe_x3.tsx, edited png, now tiles fit into x3 schema

---
+++ tilesets/Crypt-Fringe_x3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Crypt-Fringe_x3" tilewidth="32" tileheight="96">
+ <image source="../graphics/tiles/Crypt-Fringe_x3.png" width="512" height="512"/>
+</tileset>
