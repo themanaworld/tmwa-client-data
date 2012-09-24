9bbf96de5a76f2d28d2ee03cf5d8213f1e3b5508
Vincent Petithory <vincent.petithory@gmail.com>
2012-09-24 14:09:24 +0200

    Add 3 tilesets for illia maps

---
+++ tilesets/power_circle_x2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="power_circle_x2" tilewidth="32" tileheight="64">
+ <image source="../graphics/tiles/power_circle_x2.png" width="96" height="64"/>
+</tileset>
