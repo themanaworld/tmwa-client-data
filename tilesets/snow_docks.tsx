dbcdffa7c9ba81c193cc1c9f501355afd99187c8
Wombat <hpwombat@yahoo.com>
2013-07-04 10:28:34 -0400

    snow docks in nivalis, minor related fixes

---
+++ tilesets/snow_docks.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="snow_docks" tilewidth="32" tileheight="32">
+ <image source="../graphics/tiles/snow_docks.png" width="512" height="320"/>
+</tileset>
