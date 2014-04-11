ced8f58490d6ebc462aeb2ab11059f5e983e093f
Bernhardt <orgzfrgh@gmail.com>
2014-04-11 21:57:59 +0200

    Change all npcs to have a player sprite base Replaced small NPCs with tiles wherever necessary

---
+++ tilesets/evil-obelisk.tsx
@@ -0,0 +1,43 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="evil-obelisk" tilewidth="64" tileheight="128">
+ <image source="../graphics/tiles/evil-obelisk.png" width="448" height="128"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="1" duration="25"/>
+   <frame tileid="2" duration="25"/>
+   <frame tileid="3" duration="25"/>
+   <frame tileid="4" duration="25"/>
+   <frame tileid="5" duration="25"/>
+   <frame tileid="6" duration="25"/>
+   <frame tileid="5" duration="25"/>
+   <frame tileid="4" duration="25"/>
+   <frame tileid="3" duration="25"/>
+   <frame tileid="2" duration="25"/>
+   <frame tileid="1" duration="25"/>
+  </animation>
+  <properties>
+   <property name="animation-delay0" value="25"/>
+   <property name="animation-delay1" value="25"/>
+   <property name="animation-delay2" value="25"/>
+   <property name="animation-delay3" value="25"/>
+   <property name="animation-delay4" value="25"/>
+   <property name="animation-delay5" value="25"/>
+   <property name="animation-delay6" value="25"/>
+   <property name="animation-delay7" value="25"/>
+   <property name="animation-delay8" value="25"/>
+   <property name="animation-delay9" value="25"/>
+   <property name="animation-delay10" value="25"/>
+   <property name="animation-frame0" value="1"/>
+   <property name="animation-frame1" value="2"/>
+   <property name="animation-frame2" value="3"/>
+   <property name="animation-frame3" value="4"/>
+   <property name="animation-frame4" value="5"/>
+   <property name="animation-frame5" value="6"/>
+   <property name="animation-frame6" value="5"/>
+   <property name="animation-frame7" value="4"/>
+   <property name="animation-frame8" value="3"/>
+   <property name="animation-frame9" value="2"/>
+   <property name="animation-frame10" value="1"/>
+  </properties>
+ </tile>
+</tileset>
