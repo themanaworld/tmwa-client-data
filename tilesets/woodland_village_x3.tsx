d874826b24d6c723db3c446d2fb8274931d0b5e1
mekolat <mekolat@gmail.com>
2014-06-26 22:57:05 -0400

    Fix map layers in Hurnscald Added Tileset woodland_village_x3.png Fixed Tileset woodland_village.png Fixed Tileset woodland_x3.png closes #120

---
+++ tilesets/woodland_village_x3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="woodland_village_x3" tilewidth="32" tileheight="96">
+ <image source="../graphics/tiles/woodland_village_x3.png" width="704" height="192"/>
+</tileset>
