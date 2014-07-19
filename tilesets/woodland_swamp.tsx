c4c45b6d2984d8d7aa9f1febd88386269c4c2dcb
mekolat <mekolat@gmail.com>
2014-07-19 00:03:15 -0400

    fix woodland_swamp terrain

--- tilesets/woodland_swamp.tsx
+++ tilesets/woodland_swamp.tsx
@@ -2,8 +2,8 @@
 <tileset name="woodland_swamp" tilewidth="32" tileheight="32">
  <image source="../graphics/tiles/woodland_swamp.png" width="512" height="512"/>
  <terraintypes>
-  <terrain name="grass" tile="101"/>
-  <terrain name="swamp" tile="53"/>
+  <terrain name="grass" tile="-1"/>
+  <terrain name="swamp" tile="-1"/>
  </terraintypes>
  <tile id="0" terrain="0,0,0,1"/>
  <tile id="1" terrain="0,0,1,1"/>
@@ -18,16 +18,8 @@
  <tile id="34" terrain="1,0,0,0"/>
  <tile id="35" terrain="1,0,1,1"/>
  <tile id="36" terrain="0,1,1,1"/>
- <tile id="48" terrain="0,0,0,"/>
- <tile id="49" terrain="0,0,,"/>
- <tile id="50" terrain="0,0,,0"/>
- <tile id="64" terrain="0,,0,"/>
- <tile id="66" terrain=",0,,0"/>
  <tile id="67" terrain="1,1,1,0"/>
  <tile id="68" terrain="1,1,0,1"/>
- <tile id="80" terrain="0,,0,0"/>
- <tile id="81" terrain=",,0,0"/>
- <tile id="82" terrain=",0,0,0"/>
  <tile id="83" terrain="1,0,1,1"/>
  <tile id="84" terrain="0,1,1,1"/>
  <tile id="101" terrain="0,0,0,0"/>
