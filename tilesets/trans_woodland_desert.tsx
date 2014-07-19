1105720fdb16b7f8194887a541d3b50fc464a681
mekolat <mekolat@gmail.com>
2014-07-18 23:20:11 -0400

    add terrain to trans_woodland_desert

--- tilesets/trans_woodland_desert.tsx
+++ tilesets/trans_woodland_desert.tsx
@@ -1,4 +1,40 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="trans_woodland_desert" tilewidth="32" tileheight="32">
  <image source="../graphics/tiles/trans_woodland_desert.png" width="512" height="256"/>
+ <terraintypes>
+  <terrain name="grass" tile="0"/>
+  <terrain name="land" tile="18"/>
+  <terrain name="sand" tile="30"/>
+ </terraintypes>
+ <tile id="0" terrain="0,0,0,0"/>
+ <tile id="1" terrain="0,0,0,1"/>
+ <tile id="2" terrain="0,0,1,1"/>
+ <tile id="3" terrain="0,0,1,0"/>
+ <tile id="13" terrain="1,1,1,2"/>
+ <tile id="14" terrain="1,1,2,2"/>
+ <tile id="15" terrain="1,1,2,1"/>
+ <tile id="16" terrain="0,0,0,0"/>
+ <tile id="17" terrain="0,1,0,1"/>
+ <tile id="18" terrain="1,1,1,1"/>
+ <tile id="19" terrain="1,0,1,0"/>
+ <tile id="29" terrain="1,2,1,2"/>
+ <tile id="30" terrain="2,2,2,2"/>
+ <tile id="31" terrain="2,1,2,1"/>
+ <tile id="32" terrain="0,0,0,0"/>
+ <tile id="33" terrain="0,1,0,0"/>
+ <tile id="34" terrain="1,1,0,0"/>
+ <tile id="35" terrain="1,0,0,0"/>
+ <tile id="45" terrain="1,2,1,1"/>
+ <tile id="46" terrain="2,2,1,1"/>
+ <tile id="47" terrain="2,1,1,1"/>
+ <tile id="48" terrain="0,0,0,0"/>
+ <tile id="51" terrain="1,1,1,0"/>
+ <tile id="52" terrain="1,1,0,1"/>
+ <tile id="60" terrain="2,2,2,1"/>
+ <tile id="61" terrain="2,2,1,2"/>
+ <tile id="64" terrain="0,0,0,0"/>
+ <tile id="67" terrain="1,0,1,1"/>
+ <tile id="68" terrain="0,1,1,1"/>
+ <tile id="76" terrain="2,1,2,2"/>
+ <tile id="77" terrain="1,2,2,2"/>
 </tileset>
