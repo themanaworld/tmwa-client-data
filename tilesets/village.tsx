856af815325b18018be434a8b45f926e2ef0ba51
wushin <pasekei@gmail.com>
2014-08-10 12:11:35 -0500

    Base tiles for Newbington

---
+++ tilesets/village.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="village" tilewidth="32" tileheight="32">
+ <image source="../graphics/tiles/village.png" width="512" height="512"/>
+</tileset>
