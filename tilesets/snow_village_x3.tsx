b5dbf0240bda7996bbfd896e4194e05b836cc1d1
mekolat <mekolat@gmail.com>
2015-01-12 13:55:22 -0500

    remap nivalis

---
+++ tilesets/snow_village_x3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="snow_village_x3" tilewidth="32" tileheight="96">
+ <image source="../graphics/tiles/snow_village_x3.png" width="320" height="96"/>
+</tileset>
---
+++ tilesets/snow_village_x4.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="snow_village_x4" tilewidth="32" tileheight="128">
+ <image source="../graphics/tiles/snow_village_x4.png" width="320" height="128"/>
+</tileset>
