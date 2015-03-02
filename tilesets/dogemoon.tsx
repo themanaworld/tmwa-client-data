e674e683e8e1a0f679524ed6ac9ac5314de72c87
Turupawn <ahmed.hn.43@gmail.com>
2015-03-02 17:00:02 -0600

    dogemoon tileset

---
+++ tilesets/dogemoon.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="dogemoon" tilewidth="32" tileheight="32">
+ <image source="../graphics/tiles/dogemoon.png" width="512" height="512"/>
+</tileset>
