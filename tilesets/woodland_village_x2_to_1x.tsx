a71e0c767bfdb96c2ca7ba18b844ad61e9bb9da4
HoraK-FDF <horak-fdf@web.de>
2023-04-08 02:18:02 +0000

    map fixes

---
+++ tilesets/woodland_village_x2_to_1x.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="woodland_village_x2_to_x1" tilewidth="32" tileheight="32" tilecount="32" columns="16">
+ <image source="../graphics/tiles/woodland_village_x2.png" width="512" height="64"/>
+</tileset>
