0e42ba9eb63cdbbf2edaa947cec95c22401bab31
Thorbjørn Lindeijer <bjorn@lindeijer.nl>
2024-08-30 11:45:27 +0200

    Fixed the durations on the water flow animations
    Assuming the flow is supposed to run this slow.
    
    Closes mana/mana#88

--- tilesets/waterflow.tsx
+++ tilesets/waterflow.tsx
@@ -21,14 +21,14 @@
    <property name="animation-frame7" value="7"/>
   </properties>
   <animation>
-   <frame tileid="0" duration="35"/>
-   <frame tileid="1" duration="35"/>
-   <frame tileid="2" duration="35"/>
-   <frame tileid="3" duration="35"/>
-   <frame tileid="4" duration="35"/>
-   <frame tileid="5" duration="35"/>
-   <frame tileid="6" duration="35"/>
-   <frame tileid="7" duration="35"/>
+   <frame tileid="0" duration="350"/>
+   <frame tileid="1" duration="350"/>
+   <frame tileid="2" duration="350"/>
+   <frame tileid="3" duration="350"/>
+   <frame tileid="4" duration="350"/>
+   <frame tileid="5" duration="350"/>
+   <frame tileid="6" duration="350"/>
+   <frame tileid="7" duration="350"/>
   </animation>
  </tile>
  <tile id="17">
@@ -51,14 +51,14 @@
    <property name="animation-frame7" value="16"/>
   </properties>
   <animation>
-   <frame tileid="9" duration="35"/>
-   <frame tileid="10" duration="35"/>
-   <frame tileid="11" duration="35"/>
-   <frame tileid="12" duration="35"/>
-   <frame tileid="13" duration="35"/>
-   <frame tileid="14" duration="35"/>
-   <frame tileid="15" duration="35"/>
-   <frame tileid="16" duration="35"/>
+   <frame tileid="9" duration="350"/>
+   <frame tileid="10" duration="350"/>
+   <frame tileid="11" duration="350"/>
+   <frame tileid="12" duration="350"/>
+   <frame tileid="13" duration="350"/>
+   <frame tileid="14" duration="350"/>
+   <frame tileid="15" duration="350"/>
+   <frame tileid="16" duration="350"/>
   </animation>
  </tile>
  <tile id="26">
@@ -81,14 +81,14 @@
    <property name="animation-frame7" value="25"/>
   </properties>
   <animation>
-   <frame tileid="18" duration="35"/>
-   <frame tileid="19" duration="35"/>
-   <frame tileid="20" duration="35"/>
-   <frame tileid="21" duration="35"/>
-   <frame tileid="22" duration="35"/>
-   <frame tileid="23" duration="35"/>
-   <frame tileid="24" duration="35"/>
-   <frame tileid="25" duration="35"/>
+   <frame tileid="18" duration="350"/>
+   <frame tileid="19" duration="350"/>
+   <frame tileid="20" duration="350"/>
+   <frame tileid="21" duration="350"/>
+   <frame tileid="22" duration="350"/>
+   <frame tileid="23" duration="350"/>
+   <frame tileid="24" duration="350"/>
+   <frame tileid="25" duration="350"/>
   </animation>
  </tile>
  <tile id="35">
@@ -111,14 +111,14 @@
    <property name="animation-frame7" value="34"/>
   </properties>
   <animation>
-   <frame tileid="27" duration="35"/>
-   <frame tileid="28" duration="35"/>
-   <frame tileid="29" duration="35"/>
-   <frame tileid="30" duration="35"/>
-   <frame tileid="31" duration="35"/>
-   <frame tileid="32" duration="35"/>
-   <frame tileid="33" duration="35"/>
-   <frame tileid="34" duration="35"/>
+   <frame tileid="27" duration="350"/>
+   <frame tileid="28" duration="350"/>
+   <frame tileid="29" duration="350"/>
+   <frame tileid="30" duration="350"/>
+   <frame tileid="31" duration="350"/>
+   <frame tileid="32" duration="350"/>
+   <frame tileid="33" duration="350"/>
+   <frame tileid="34" duration="350"/>
   </animation>
  </tile>
  <tile id="44">
@@ -141,14 +141,14 @@
    <property name="animation-frame7" value="43"/>
   </properties>
   <animation>
-   <frame tileid="36" duration="35"/>
-   <frame tileid="37" duration="35"/>
-   <frame tileid="38" duration="35"/>
-   <frame tileid="39" duration="35"/>
-   <frame tileid="40" duration="35"/>
-   <frame tileid="41" duration="35"/>
-   <frame tileid="42" duration="35"/>
-   <frame tileid="43" duration="35"/>
+   <frame tileid="36" duration="350"/>
+   <frame tileid="37" duration="350"/>
+   <frame tileid="38" duration="350"/>
+   <frame tileid="39" duration="350"/>
+   <frame tileid="40" duration="350"/>
+   <frame tileid="41" duration="350"/>
+   <frame tileid="42" duration="350"/>
+   <frame tileid="43" duration="350"/>
   </animation>
  </tile>
  <tile id="53">
@@ -171,14 +171,14 @@
    <property name="animation-frame7" value="52"/>
   </properties>
   <animation>
-   <frame tileid="45" duration="35"/>
-   <frame tileid="46" duration="35"/>
-   <frame tileid="47" duration="35"/>
-   <frame tileid="48" duration="35"/>
-   <frame tileid="49" duration="35"/>
-   <frame tileid="50" duration="35"/>
-   <frame tileid="51" duration="35"/>
-   <frame tileid="52" duration="35"/>
+   <frame tileid="45" duration="350"/>
+   <frame tileid="46" duration="350"/>
+   <frame tileid="47" duration="350"/>
+   <frame tileid="48" duration="350"/>
+   <frame tileid="49" duration="350"/>
+   <frame tileid="50" duration="350"/>
+   <frame tileid="51" duration="350"/>
+   <frame tileid="52" duration="350"/>
   </animation>
  </tile>
  <tile id="62">
@@ -201,14 +201,14 @@
    <property name="animation-frame7" value="61"/>
   </properties>
   <animation>
-   <frame tileid="54" duration="35"/>
-   <frame tileid="55" duration="35"/>
-   <frame tileid="56" duration="35"/>
-   <frame tileid="57" duration="35"/>
-   <frame tileid="58" duration="35"/>
-   <frame tileid="59" duration="35"/>
-   <frame tileid="60" duration="35"/>
-   <frame tileid="61" duration="35"/>
+   <frame tileid="54" duration="350"/>
+   <frame tileid="55" duration="350"/>
+   <frame tileid="56" duration="350"/>
+   <frame tileid="57" duration="350"/>
+   <frame tileid="58" duration="350"/>
+   <frame tileid="59" duration="350"/>
+   <frame tileid="60" duration="350"/>
+   <frame tileid="61" duration="350"/>
   </animation>
  </tile>
  <tile id="71">
@@ -231,14 +231,14 @@
    <property name="animation-frame7" value="70"/>
   </properties>
   <animation>
-   <frame tileid="63" duration="35"/>
-   <frame tileid="64" duration="35"/>
-   <frame tileid="65" duration="35"/>
-   <frame tileid="66" duration="35"/>
-   <frame tileid="67" duration="35"/>
-   <frame tileid="68" duration="35"/>
-   <frame tileid="69" duration="35"/>
-   <frame tileid="70" duration="35"/>
+   <frame tileid="63" duration="350"/>
+   <frame tileid="64" duration="350"/>
+   <frame tileid="65" duration="350"/>
+   <frame tileid="66" duration="350"/>
+   <frame tileid="67" duration="350"/>
+   <frame tileid="68" duration="350"/>
+   <frame tileid="69" duration="350"/>
+   <frame tileid="70" duration="350"/>
   </animation>
  </tile>
 </tileset>
