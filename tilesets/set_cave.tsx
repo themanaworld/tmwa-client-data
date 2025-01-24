5c84f0cc83f51b8ca0b9ba1f90e40092bafd7c45
Thorbjørn Lindeijer <bjorn@lindeijer.nl>
2025-01-24 21:32:46 +0100

    Moved the "set_" tilesets among the other tilesets
    No real reason to separate them to their own folder.

--- tilesets/set_cave.tsx
+++ tilesets/set_cave.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.0" name="set_cave" tilewidth="32" tileheight="32" spacing="1" tilecount="49" columns="7">
- <image source="../../graphics/tiles/set_cave.png" width="256" height="256"/>
+ <image source="../graphics/tiles/set_cave.png" width="256" height="256"/>
 </tileset>
--- tilesets/set_rules.tsx
+++ tilesets/set_rules.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.11" tiledversion="1.11.1" name="set_rules" tilewidth="32" tileheight="32" tilecount="4" columns="2">
- <image source="../../graphics/tiles/set_rules.png" width="64" height="64"/>
+ <image source="../graphics/tiles/set_rules.png" width="64" height="64"/>
 </tileset>
