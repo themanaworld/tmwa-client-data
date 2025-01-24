74624ca46e56a2e8fc38dafa23eed20192c647bb
Thorbjørn Lindeijer <bjorn@lindeijer.nl>
2025-01-24 15:51:00 +0100

    Replaced many internal tilesets with external tilesets
    Only exhaustive for woodland rules for now.
    
    Restored graphics/tiles/set_woodland.png from git history.

---
+++ rules/tile_sets/set_woodland.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.11.1" name="set_woodland" tilewidth="32" tileheight="32" tilecount="160" columns="16">
+ <image source="../../graphics/tiles/set_woodland.png" width="512" height="320"/>
+</tileset>
