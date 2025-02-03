caa0abb8db7d54f3469f1e9cc1a362f3db1a3ae7
AJ Keresztes <aj@keresztes.us>
2025-02-03 15:32:34 +0100

    added tilesets for autogenerated maps

---
+++ rules/tile_sets/set_rules.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="set_rules" tilewidth="32" tileheight="32" spacing="1" tilecount="1" columns="1">
+ <image source="../../graphics/tiles/set_rules.png" width="64" height="64"/>
+</tileset>
